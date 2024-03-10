dc17a42d0f0679475d99d2c5d304983c12dcfdbc
gleb <webrushgithub@gmail.com>
2024-03-10 17:26:27 +0500

    Сделал доп

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import telebot
+import os
 from config import *
 from logic import *
 bot = telebot.TeleBot(token)
 
 
+@bot.message_handler(commands=['start'])
+def info(message):
+    bot.send_message(
+        message.chat.id, 'Привет! Я бот для генерации изображений! Напиши свой запрос в чат и ожидай фото! 🥰')
+
+
+def delete_message(chat_id, message_id, delay):
+    time.sleep(delay)
+    bot.delete_message(chat_id, message_id)
+
+
 @bot.message_handler(func=lambda message: True)
 def start(message):
+    greeting_message = bot.reply_to(message, 'Генерирую картинку...')
+    greeting_message_id = greeting_message.message_id
+    delete_message(message.chat.id, greeting_message_id, 30)
+    bot.send_chat_action(message.chat.id, 'typing')
     prompt = message.text
     user_id = message.from_user.id
     path = f'images/{user_id}.png'
@@ -18,6 +34,9 @@ def start(message):
 
     with open(path, 'rb') as photo:
         bot.send_photo(message.chat.id, photo)
+
+    if os.path.isfile(path):
+        os.remove(path)
 
 
 bot.infinity_polling()
